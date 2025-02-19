488e4718b6051027039f8028f6ff6cf53524f575
Tyler Leggio <tyler.leggio>
2025-02-19 10:59:34 -0600

    Added UserRoles

---
+++ Selu383.SP25.P02.Api/Features/Identity/UserRoles.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Selu383.SP25.P02.Api.Features.Identity
+{
+    public class UserRoles : IdentityUserRole<int>
+    {
+        public User User { get; set; }
+        public Role Role { get; set; }
+    }
+}
