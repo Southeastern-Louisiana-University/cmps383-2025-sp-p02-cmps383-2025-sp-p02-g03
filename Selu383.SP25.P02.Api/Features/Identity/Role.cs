9293f55944da724a7d3925ad77f8d564feda0896
Peyton Perry <peyton.perry@selu.edu>
2025-02-19 17:21:09 -0600

    DataContext error fix
    In the Roles I had IdentityUser where it should have been IdentityRole

--- Selu383.SP25.P02.Api/Features/Identity/Role.cs
+++ Selu383.SP25.P02.Api/Features/Identity/Role.cs
@@ -3,7 +3,7 @@
 
 namespace Selu383.SP25.P02.Api.Features.Identity
 {
-    public class Role : IdentityUser<int>
+    public class Role : IdentityRole<int>
     {
         public virtual ICollection<UserRoles> Users { get; set; } = new List<UserRoles>();
     }
