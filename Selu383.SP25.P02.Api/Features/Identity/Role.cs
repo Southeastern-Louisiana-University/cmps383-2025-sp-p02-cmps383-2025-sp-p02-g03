2d211a05c227642f7d4fba1bab0a587f073f80d5
Tyler Leggio <145399109+T-Leg1@users.noreply.github.com>
2025-02-24 17:28:08 -0600

    UsersController

--- Selu383.SP25.P02.Api/Features/Identity/Role.cs
+++ Selu383.SP25.P02.Api/Features/Identity/Role.cs
@@ -5,6 +5,6 @@ namespace Selu383.SP25.P02.Api.Features.Identity
 {
     public class Role : IdentityRole<int>
     {
-        public virtual ICollection<UserRoles> Users { get; set; } = new List<UserRoles>();
+        public virtual ICollection<UserRoles> User { get; set; } = new List<UserRoles>();
     }
 }
